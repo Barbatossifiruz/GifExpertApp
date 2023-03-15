472b128c6f673c9268e7d910853e3948a156cec6
Barbatossifiruz <makotohishigami@gmail.com>
2023-03-14 21:02:11 -0400

    Update gifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+//* Importar UseState
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(["One Punch", " Dragon Ball"]);
+
+    console.log(categories);
+
     return (
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+            {/* Input */}
+            {/* Listado de Gif */}
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}> {category}</li>;
+                })}
+                {/* <li>123</li> */}
+            </ol>
+            {/* Gif items */}
         </>
     );
 };

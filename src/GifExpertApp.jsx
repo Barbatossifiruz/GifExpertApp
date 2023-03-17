befb2b0df0e5fe10f6b07c328f31934dc98d52ba
Barbatossifiruz <makotohishigami@gmail.com>
2023-03-17 13:20:32 -0400

    update GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,23 +1,41 @@
 //* Importar UseState
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch", " Dragon Ball"]);
 
-    console.log(categories);
+    const onAddCategory = (newCategory) => {
+        if (categories.some((category) => category.toLowerCase() === newCategory.toLowerCase())) {
+            return;
+        }
+        setCategories([newCategory, ...categories]);
+    };
 
     return (
         <>
             {/* Titulo */}
+
             <h1>GifExpertApp</h1>
+
             {/* Input */}
+
+            <AddCategory
+                // setCategories={setCategories}
+                onNewCategory={(value) => onAddCategory(value)}
+            />
+
             {/* Listado de Gif */}
+
             <ol>
-                {categories.map((category) => {
-                    return <li key={category}> {category}</li>;
-                })}
-                {/* <li>123</li> */}
+                {categories.map((category) => (
+                    <div key={category}>
+                        <h3>{category}</h3>
+                        <li> {category} </li>
+                    </div>
+                ))}
             </ol>
+
             {/* Gif items */}
         </>
     );

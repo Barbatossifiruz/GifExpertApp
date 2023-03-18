0bbbe3f74ae1c74d811866db58c3a6e305a1cad3
Barbatossifiruz <makotohishigami@gmail.com>
2023-03-17 21:24:18 -0400

    Update GifGrid

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGif";
+
 export const GifGrid = ({ category }) => {
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+
+        setImages(newImages);
+    };
+
+    useEffect(() => {
+        getImages();
+    }, []);
+
     return (
         <>
             <h3>{category}</h3>
+
+            <ol>
+                {images.map(({ id, title }) => (
+                    <li key={id}> {title}</li>
+                ))}
+            </ol>
         </>
     );
 };

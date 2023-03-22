124e44be098a43ef1b20b70d6ce3858200280910
Barbatossifiruz <makotohishigami@gmail.com>
2023-03-22 11:22:54 -0400

    update Gifgrid

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGif";
+/*
+//! Orden de importaciones
+ *1 - React
+ *2- Importaciones de terceros
+ *3- Nuestro codigo
+ *4- Funciones
+*/
+
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { Loading } from "./Loading";
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
-
-    const getImages = async () => {
-        const newImages = await getGifs(category);
-
-        setImages(newImages);
-    };
-
-    useEffect(() => {
-        getImages();
-    }, []);
+    const { images, isLoading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+            <Loading isLoading={isLoading} />
 
-            <ol>
-                {images.map(({ id, title }) => (
-                    <li key={id}> {title}</li>
+            <div className="card-grid">
+                {images.map((image) => (
+                    <GifItem key={image.id} {...image} />
                 ))}
-            </ol>
+            </div>
         </>
     );
 };
